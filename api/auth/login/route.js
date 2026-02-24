a0c82683e2c6c4027e7b6ec80fbe921b89d7e517
achu <achuabi2419@gmail.com>
2026-02-24 21:43:53 +0530

    Create route.js

---
+++ api/auth/login/route.js
@@ -0,0 +1,28 @@
+import { NextResponse } from "next/server";
+import bcrypt from "bcryptjs";
+import jwt from "jsonwebtoken";
+import { connectDB } from "@/lib/mongodb";
+import User from "@/models/User";
+
+export async function POST(req) {
+  await connectDB();
+  const { email, password } = await req.json();
+
+  const user = await User.findOne({ email });
+
+  if (!user)
+    return NextResponse.json({ error: "User not found" }, { status: 404 });
+
+  const isMatch = await bcrypt.compare(password, user.password);
+
+  if (!isMatch)
+    return NextResponse.json({ error: "Invalid credentials" }, { status: 401 });
+
+  const token = jwt.sign(
+    { id: user._id },
+    process.env.JWT_SECRET,
+    { expiresIn: "7d" }
+  );
+
+  return NextResponse.json({ token });
+}
